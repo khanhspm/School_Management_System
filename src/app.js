3a278074fbb4a46e048d925017fd8d5e6c565460
khanhspm <khanhcshadu@gmail.com>
2023-04-11 22:23:06 +0700

    first layout of web

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const app = express()
+const port = 3000
+
+// HTTP local
+app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Template engine
+app.engine('.hdb', handlebars.engine({
+  extname: '.hdb'
+}));
+app.set('view engine', '.hdb') 
+app.set('views', path.join(__dirname, 'resources\\views')) 
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/infor', (req, res) => {
+  res.render('information')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
